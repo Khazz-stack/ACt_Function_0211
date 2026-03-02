d107e68420fdc02fd206e94e8098e9c0aec04b05
Khazz <thelenaid@gmail.com>
2026-03-02 21:19:45 +0700

    implementasi

--- fungsidenganparameter.cpp
+++ fungsidenganparameter.cpp
@@ -3,3 +3,19 @@
 using namespacestd;
 //deklarasi variable global
 int panjang, lebar;
+//implementasi fungsi dan prosedur
+void input(){
+    cout << "Masukan Panjang : ";
+    cin >> panjang;
+    cout << "Masukan Lebar : ";
+    cin >> lebar;
+}
+int luaspersegi(int a,int b){
+    return a * b;
+}
+int jumlah(int x, int y, intz){
+    return x+y+z;
+}
+void output(){
+    cout << "Hasilnya : " << luaspersegi(panjang,lebar) << endl;
+}
